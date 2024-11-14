cbe8e8706124311cb0fac7ec309ab6a1611264d3
Sébastien Dumont <mailme@sebastiendumont.com>
2024-11-14 16:40:02 +0100

    Tweaks

--- bin/github-updater-prep.sh
+++ bin/github-updater-prep.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # github-updater-prep.sh
-# Usage: ./github-updater-prep.sh path/to/main-plugin-file.php "owner/repository"
+# Usage: ./github-updater-prep.sh "path/to/main-plugin-file" "plugin-slug" "1.0.0" "owner/repository"
 
-MAIN_FILE="$1"
-REPO_NAME="$2"
+MAIN_FILE_LOCATION="$1"
+PLUGIN_NAME="$2"
+VERSION="$3"
+REPO_NAME="$4"
 
 # Define the new headers to add
 NEW_HEADERS=$(cat <<-END
  * GitHub Plugin URI: $REPO_NAME
+ * Primary Branch: trunk
+ * Release Asset: ${PLUGIN_NAME}-github-v${VERSION}.zip
 END
 )
 
 # Backup the original file
-cp "$MAIN_FILE" "${MAIN_FILE}.bak"
+cp "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.php" "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.bak"
 
 # Check if the headers are already present to avoid duplicates
-if ! grep -q "GitHub Plugin URI: $REPO_NAME" "$MAIN_FILE"; then
+if ! grep -q "GitHub Plugin URI: $REPO_NAME" "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.php"; then
   # Use awk to insert the new headers right after "Requires PHP" line without adding an extra blank line
   awk -v new_headers="$NEW_HEADERS" '
     BEGIN { added = 0 }
@@ -26,10 +30,10 @@ if ! grep -q "GitHub Plugin URI: $REPO_NAME" "$MAIN_FILE"; then
       next
     }
     { print $0 }  # Print the rest of the file as is
-  ' "$MAIN_FILE" > "${MAIN_FILE}.tmp" && mv "${MAIN_FILE}.tmp" "$MAIN_FILE"
+  ' "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.php" > "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.tmp" && mv "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.tmp" "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.php"
 else
-  echo "Headers already present in $MAIN_FILE"
+  echo "Headers already present in ${MAIN_FILE_LOCATION}${PLUGIN_NAME}.php"
 fi
 
 # Delete the backup file after successful modification
-rm -f "${MAIN_FILE}.bak"
+rm -f "${MAIN_FILE_LOCATION}${PLUGIN_NAME}.bak"
