3a28d2e97dd3fb1e9180f1e68d4df63e0006cea8
Sebastien <sebastiendumont1986@gmail.com>
2025-02-27 18:01:17 +0000

    Removing as it will prevent developers from registering custom routes.

--- includes/classes/rest-api/class-cocart-rest-api.php
+++ includes/classes/rest-api/class-cocart-rest-api.php
@@ -142,13 +142,6 @@ class CoCart_REST_API {
 			)
 		);
 
-		// Check if we should load the requested namespace.
-		foreach ( $namespaces as $namespace => $controller ) {
-			if ( ! cocart_rest_should_load_namespace( $namespace ) ) {
-				unset( $namespaces[ $namespace ] );
-			}
-		}
-
 		return $namespaces;
 	} // END get_rest_namespaces()
 
